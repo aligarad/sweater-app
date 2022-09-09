46e63f92f5415c6c07f657a0816bc895847cce7e
kenhunja <garadcali@gmail.com>
2022-09-09 23:39:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -32,3 +32,17 @@ document.querySelector(".search-box").addEventListener('keyup', function (event)
   }
 }
 )
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+let month = months[d.getMonth()];
+document.getElementById("month").innerHTML = month;
+
+const p= new Date();
+document.getElementById("date").innerHTML = d.getDate();
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const z = new Date();
+let day = days[d.getDay()];
+document.getElementById("demo").innerHTML = day;
